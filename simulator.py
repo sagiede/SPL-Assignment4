8649d716c2cbd120dd9fd26102eb063017334ea0
Shalev Shalit <shalevsh@wix.com>
2018-01-18 22:25:08 +0200

    first table created

--- simulator.py
+++ simulator.py
@@ -1,2 +1,13 @@
 import sqlite3
-dbcon = sqlite3.connect('example.db')
+dbcon = sqlite3.connect('world.db')
+
+with dbcon:
+    cursor = dbcon.cursor()
+    cursor.execute("""CREATE TABLE tasks(
+                    id INTEGER PRIMARY KEY, 
+                    task_name TEXT NOT NULL, 
+                    worker_id INTEGER REFERENCES workers(id), 
+                    time_to_make INTEGER NOT NULL, 
+                    resource_name TEXT NOT NULL, 
+                    resource_amount INTEGER NOT NULL
+                   )""")
